3181aec1e27cdc9c3e8da6b8b0dd90f5d28d05d4
brsneed <70169788+brsneed@users.noreply.github.com>
2020-09-17 00:49:35 -0400

    Updated Hello world to say "hello from github"

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello From Eclipse");
-		System.out.print("Hello World");
+		System.out.print("Hello from git hub");
 
 	}
 
